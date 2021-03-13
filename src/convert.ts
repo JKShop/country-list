adb57de838058bb19693f645db96997efca45e36
Scarjit <ferdinand.linnenberg@aol.com>
2021-03-13 18:15:05 +0000

    fix: Changed countries.ts to be compatible with country.interface.ts

--- src/convert.ts
+++ src/convert.ts
@@ -1,6 +1,13 @@
-import { countries } from "./data/countries.ts";
+import countries from "./data/countries.ts";
+import {Country} from "./country.interface.ts";
 
-let json_countries = JSON.stringify(countries);
+let JsonCountries = JSON.stringify(countries);
 
-const write = Deno.writeTextFile("countries.json", json_countries);
+const write = Deno.writeTextFile("countries.json", JsonCountries);
 write.then(() => console.log("File written to countries.txt"));
+
+let SqlHeader = "INSERT INTO core_countries (id, name, iso2, iso3, continent, lat, long) VALUES";
+
+countries.forEach((c: Country) => {
+    console.log(c)
+});
